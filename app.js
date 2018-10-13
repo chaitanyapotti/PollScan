de432e3feb7b1a4cec46ea681eef5a054be38832
Vinay Bagul <vinay@yun.buzz>
2018-10-13 23:29:47 +0530

    new ABI, time APIs and denominator API

--- app.js
+++ app.js
@@ -6,37 +6,95 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 const Web3 = require('web3');
 const HDWalletProvider = require("truffle-hdwallet-provider");
-const testnet = `https://kovan.infura.io/v3/894765ed26c2470587b00e37249612e4`
+const testnet = `https://rinkeby.infura.io/v3/894765ed26c2470587b00e37249612e4`
 
 const provider = new HDWalletProvider(
   // "blue inherit drum enroll amused please camp false estate flash sell right",
   process.env.ACCOUNT_WORDS,
   testnet
 );
-abi = [
-  {
+abi= [
+    {
       "anonymous": false,
       "inputs": [
-          {
-              "indexed": true,
-              "name": "_from",
-              "type": "address"
-          },
-          {
-              "indexed": true,
-              "name": "_to",
-              "type": "uint256"
-          },
-          {
-              "indexed": false,
-              "name": "voteWeight",
-              "type": "uint256"
-          }
+        {
+          "indexed": true,
+          "name": "_from",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "name": "_to",
+          "type": "uint8"
+        },
+        {
+          "indexed": false,
+          "name": "voteWeight",
+          "type": "uint256"
+        }
+      ],
+      "name": "TriedToVote",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "name": "_from",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "name": "_to",
+          "type": "uint8"
+        },
+        {
+          "indexed": false,
+          "name": "voteWeight",
+          "type": "uint256"
+        }
+      ],
+      "name": "CastVote",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "name": "_from",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "name": "_to",
+          "type": "uint8"
+        },
+        {
+          "indexed": false,
+          "name": "voteWeight",
+          "type": "uint256"
+        }
       ],
       "name": "RevokedVote",
       "type": "event"
-  },
-  {
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "_proposalId",
+          "type": "uint8"
+        }
+      ],
+      "name": "vote",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
       "constant": false,
       "inputs": [],
       "name": "revokeVote",
@@ -44,389 +102,238 @@ abi = [
       "payable": false,
       "stateMutability": "nonpayable",
       "type": "function"
-  },
-  {
-      "anonymous": false,
-      "inputs": [
-          {
-              "indexed": true,
-              "name": "_from",
-              "type": "address"
-          },
-          {
-              "indexed": true,
-              "name": "_to",
-              "type": "uint256"
-          },
-          {
-              "indexed": false,
-              "name": "voteWeight",
-              "type": "uint256"
-          }
-      ],
-      "name": "TriedToVote",
-      "type": "event"
-  },
-  {
-      "anonymous": false,
-      "inputs": [
-          {
-              "indexed": true,
-              "name": "_from",
-              "type": "address"
-          },
-          {
-              "indexed": true,
-              "name": "_to",
-              "type": "uint256"
-          },
-          {
-              "indexed": false,
-              "name": "voteWeight",
-              "type": "uint256"
-          }
-      ],
-      "name": "CastVote",
-      "type": "event"
-  },
-  {
-      "constant": false,
-      "inputs": [
-          {
-              "name": "_proposal",
-              "type": "uint8"
-          }
-      ],
-      "name": "vote",
-      "outputs": [],
-      "payable": false,
-      "stateMutability": "nonpayable",
-      "type": "function"
-  },
-  {
-      "inputs": [
-          {
-              "name": "_protocolAddresses",
-              "type": "address[]"
-          },
-          {
-              "name": "_proposalNames",
-              "type": "bytes32[]"
-          }
-      ],
-      "payable": false,
-      "stateMutability": "nonpayable",
-      "type": "constructor"
-  },
-  {
-      "constant": true,
-      "inputs": [
-          {
-              "name": "_to",
-              "type": "address"
-          }
-      ],
-      "name": "calculateVoteWeight",
-      "outputs": [
-          {
-              "name": "",
-              "type": "uint256"
-          }
-      ],
-      "payable": false,
-      "stateMutability": "view",
-      "type": "function"
-  },
-  {
-      "constant": true,
-      "inputs": [
-          {
-              "name": "_to",
-              "type": "address"
-          }
-      ],
-      "name": "canVote",
-      "outputs": [
-          {
-              "name": "",
-              "type": "bool"
-          }
-      ],
-      "payable": false,
-      "stateMutability": "view",
-      "type": "function"
-  },
-  {
-      "constant": true,
-      "inputs": [],
-      "name": "getName",
-      "outputs": [
-          {
-              "name": "",
-              "type": "string"
-          }
-      ],
-      "payable": false,
-      "stateMutability": "view",
-      "type": "function"
-  },
-  {
-      "constant": true,
-      "inputs": [],
-      "name": "getPollType",
-      "outputs": [
-          {
-              "name": "",
-              "type": "string"
-          }
-      ],
-      "payable": false,
-      "stateMutability": "view",
-      "type": "function"
-  },
-  {
+    },
+    {
       "constant": true,
       "inputs": [],
       "name": "getProposals",
       "outputs": [
-          {
-              "name": "",
-              "type": "bytes32[]"
-          }
+        {
+          "name": "",
+          "type": "bytes32[]"
+        }
       ],
       "payable": false,
       "stateMutability": "view",
       "type": "function"
-  },
-  {
-      "constant": true,
-      "inputs": [],
-      "name": "getProtocolAddresses",
-      "outputs": [
-          {
-              "name": "",
-              "type": "address[]"
-          }
-      ],
-      "payable": false,
-      "stateMutability": "view",
-      "type": "function"
-  },
-  {
-      "constant": true,
-      "inputs": [],
-      "name": "getVoterBaseLogic",
-      "outputs": [
-          {
-              "name": "",
-              "type": "string"
-          }
-      ],
-      "payable": false,
-      "stateMutability": "view",
-      "type": "function"
-  },
-  {
+    },
+    {
       "constant": true,
       "inputs": [
-          {
-              "name": "_proposalId",
-              "type": "uint256"
-          }
+        {
+          "name": "_to",
+          "type": "address"
+        }
       ],
-      "name": "getVoterCount",
+      "name": "canVote",
       "outputs": [
-          {
-              "name": "",
-              "type": "uint256"
-          }
+        {
+          "name": "",
+          "type": "bool"
+        }
       ],
       "payable": false,
       "stateMutability": "view",
       "type": "function"
-  },
-  {
-      "constant": true,
-      "inputs": [],
-      "name": "getVoterCounts",
-      "outputs": [
-          {
-              "name": "",
-              "type": "uint256[]"
-          }
-      ],
-      "payable": false,
-      "stateMutability": "view",
-      "type": "function"
-  },
-  {
-      "constant": true,
-      "inputs": [],
-      "name": "getVoteTallies",
-      "outputs": [
-          {
-              "name": "",
-              "type": "uint256[]"
-          }
-      ],
-      "payable": false,
-      "stateMutability": "view",
-      "type": "function"
-  },
-  {
+    },
+    {
       "constant": true,
       "inputs": [
-          {
-              "name": "_proposalId",
-              "type": "uint256"
-          }
+        {
+          "name": "_proposalId",
+          "type": "uint256"
+        }
       ],
       "name": "getVoteTally",
       "outputs": [
-          {
-              "name": "",
-              "type": "uint256"
-          }
+        {
+          "name": "",
+          "type": "uint256"
+        }
       ],
       "payable": false,
       "stateMutability": "view",
       "type": "function"
-  },
-  {
-      "constant": true,
-      "inputs": [],
-      "name": "pollName",
-      "outputs": [
-          {
-              "name": "",
-              "type": "string"
-          }
-      ],
-      "payable": false,
-      "stateMutability": "view",
-      "type": "function"
-  },
-  {
-      "constant": true,
-      "inputs": [],
-      "name": "pollType",
-      "outputs": [
-          {
-              "name": "",
-              "type": "string"
-          }
-      ],
-      "payable": false,
-      "stateMutability": "view",
-      "type": "function"
-  },
-  {
+    },
+    {
       "constant": true,
       "inputs": [
-          {
-              "name": "",
-              "type": "uint256"
-          }
+        {
+          "name": "_proposalId",
+          "type": "uint256"
+        }
       ],
-      "name": "proposals",
+      "name": "getVoterCount",
       "outputs": [
-          {
-              "name": "voteCount",
-              "type": "uint256"
-          },
-          {
-              "name": "voteWeight",
-              "type": "uint256"
-          },
-          {
-              "name": "name",
-              "type": "bytes32"
-          }
+        {
+          "name": "",
+          "type": "uint256"
+        }
       ],
       "payable": false,
       "stateMutability": "view",
       "type": "function"
-  },
-  {
+    },
+    {
       "constant": true,
       "inputs": [
-          {
-              "name": "",
-              "type": "uint256"
-          }
+        {
+          "name": "_to",
+          "type": "address"
+        }
       ],
-      "name": "protocolAddresses",
+      "name": "calculateVoteWeight",
       "outputs": [
-          {
-              "name": "",
-              "type": "address"
-          }
+        {
+          "name": "",
+          "type": "uint256"
+        }
       ],
       "payable": false,
       "stateMutability": "view",
       "type": "function"
-  },
-  {
-      "constant": true,
-      "inputs": [],
-      "name": "voterBaseLogic",
-      "outputs": [
-          {
-              "name": "",
-              "type": "string"
-          }
-      ],
-      "payable": false,
-      "stateMutability": "view",
-      "type": "function"
-  },
-  {
-      "constant": true,
-      "inputs": [
-          {
-              "name": "",
-              "type": "address"
-          }
-      ],
-      "name": "voters",
-      "outputs": [
-          {
-              "name": "voted",
-              "type": "bool"
-          },
-          {
-              "name": "vote",
-              "type": "uint8"
-          },
-          {
-              "name": "delegate",
-              "type": "address"
-          },
-          {
-              "name": "weight",
-              "type": "uint256"
-          }
-      ],
-      "payable": false,
-      "stateMutability": "view",
-      "type": "function"
-  },
-  {
+    },
+    {
       "constant": true,
       "inputs": [],
       "name": "winningProposal",
       "outputs": [
-          {
-              "name": "",
-              "type": "uint8"
-          }
+        {
+          "name": "",
+          "type": "uint8"
+        }
       ],
       "payable": false,
       "stateMutability": "view",
       "type": "function"
-  }
-]
-
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "getName",
+      "outputs": [
+        {
+          "name": "",
+          "type": "bytes32"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "getPollType",
+      "outputs": [
+        {
+          "name": "",
+          "type": "bytes32"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "getVoterBaseLogic",
+      "outputs": [
+        {
+          "name": "",
+          "type": "bytes32"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "getStartTime",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "getEndTime",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "getProtocolAddresses",
+      "outputs": [
+        {
+          "name": "",
+          "type": "address[]"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "getVoteTallies",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256[]"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "getVoterCounts",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256[]"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "getVoterBaseDenominator",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ]
 web3 = new Web3(provider);
 global.contractInstances = {}
 
--- routes/pollscan.js
+++ routes/pollscan.js
@@ -17,7 +17,7 @@ router.get("/name", function (req, res, next) {
                 console.error(err);
                 res.status(500).json({ message: "Failed", reason: err });
             }
-            res.json({ message: "Success", data: { name: result } });
+            res.json({ message: "Success", data: { name: web3.utils.hexToAscii(result).replace(/\0/g, '') } });
         });
     } else {
         res
@@ -43,7 +43,7 @@ router.get("/polltype", function (req, res, next) {
                 console.error(err);
                 res.status(500).json({ message: "Failed", reason: err });
             }
-            res.json({ message: "Success", data: { polltype: result } });
+            res.json({ message: "Success", data: { polltype: web3.utils.hexToAscii(result).replace(/\0/g, '') } });
         });
     } else {
         res
@@ -64,78 +64,58 @@ router.get("/events", function (req, res, next) {
             var contractInstance = new web3.eth.Contract(abi, votingContractAddress);
             global.contractInstances[votingContractAddress] = contractInstance;
         }
-        var myEvents = contractInstance.getPastEvents(
-            { fromBlock: 0, toBlock: "latest" },
+        contractInstance.getPastEvents({ fromBlock: "3000000", toBlock: 'latest' },
             function (err, logs) {
                 if (err) {
-                    console.log(err);
+                    console.log("printing error: ", err)
+                    res.status(500).json({ 'message': 'Failed', 'reason': err.message })
+                    return
                 }
-                contractInstance.getPastEvents({ fromBlock: 0, toBlock: 'latest' },
-                    function (err, logs) {
-                        if (err) {
-                            console.log(err)
-                            res.status(500).json({ 'message': 'Failed', 'reason': err })
-                        }
-                        console.log('logs: ', logs)
-                        if (logs.length > 0) {
-                            getFirstAndLastBlockTimeStamp = async () => {
-                                let firstBlockDetails = await web3.eth.getBlock(logs[0]['blockNumber'])
-                                let lastBlockDetails = await web3.eth.getBlock(logs[logs.length - 1]['blockNumber'])
-                                // console.log(firstBlockDetails, lastBlockDetails)
-                                let activitiesArray = []
-                                let m; let b;
-                                if (firstBlockDetails.number === lastBlockDetails.number) {
-                                    m = 0
-                                    b = lastBlockDetails.timestamp
-                                } else {
-                                    m = (lastBlockDetails.timestamp - firstBlockDetails.timestamp) / (firstBlockDetails.number - lastBlockDetails.number)
-                                    b = lastBlockDetails.timestamp - m * lastBlockDetails.number
-                                }
-
-                                for (let log of logs) {
-                                    activitiesArray.push({
-                                        type: log.event, weight: log.returnValues.voteWeight,
-                                        value: log.returnValues._to, address: log.returnValues._from, datetime: parseInt(m * log.blockNumber + b)
-                                    })
-                                }
-                                // blocktimes: { firstblockno: firstBlockDetails.number, firstblocktime: firstBlockDetails.timestamp, lastblockno: lastBlockDetails.number, lastblocktime: lastBlockDetails.timestamp }
-                                res.send({ message: 'Success', data: { events: activitiesArray } })
-                            }
-                            getFirstAndLastBlockTimeStamp()
-
-                            // for (let log of logs){
-                            //     console.log(log)
-                            //     web3.eth.getBlock(log['blockNumber'], function(err, data){
-                            //         console.log(data)
-                            //     })
-                            // }
+                // console.log('logs: ', logs)
+                if (logs.length > 0) {
+                    getFirstAndLastBlockTimeStamp = async () => {
+                        let firstBlockDetails = await web3.eth.getBlock(logs[0]['blockNumber'])
+                        let lastBlockDetails = await web3.eth.getBlock(logs[logs.length - 1]['blockNumber'])
+                        // console.log(firstBlockDetails, lastBlockDetails)
+                        let activitiesArray = []
+                        let m; let b;
+                        if (firstBlockDetails.number === lastBlockDetails.number) {
+                            m = 0
+                            b = lastBlockDetails.timestamp
                         } else {
-                            res.json({ 'message': 'Failed', 'reason': 'No logs available at the moment.' })
+                            m = (lastBlockDetails.timestamp - firstBlockDetails.timestamp) / (firstBlockDetails.number - lastBlockDetails.number)
+                            b = lastBlockDetails.timestamp - m * lastBlockDetails.number
                         }
-                    })
 
-                // for (let log of logs){
-                //     console.log(log)
-                //     web3.eth.getBlock(log['blockNumber'], function(err, data){
-                //         console.log(data)
-                //     })
-                // }
-            }
+                        for (let log of logs) {
+                            activitiesArray.push({
+                                type: log.event, weight: log.returnValues.voteWeight,
+                                value: log.returnValues._to, address: log.returnValues._from, datetime: parseInt(m * log.blockNumber + b)
+                            })
+                        }
+                        // blocktimes: { firstblockno: firstBlockDetails.number, firstblocktime: firstBlockDetails.timestamp, lastblockno: lastBlockDetails.number, lastblocktime: lastBlockDetails.timestamp }
+                        res.send({ message: 'Success', data: { events: activitiesArray } })
+                    }
+                    getFirstAndLastBlockTimeStamp()
 
-        );
-
-        // contractInstance.methods.getPollType().call(function (err, result) {
-        //     if (err) { console.error(err); res.status(500).json({ 'message': 'Failed', 'reason': err }) };
-        //     res.json({ message: 'Success', data: { polltype: result } })
-        // })
-    } else {
-        res
-            .status(500)
-            .json({
-                message: "Failed",
-                reason: "Contract Address is missing in the query"
-            });
-    }
+                    // for (let log of logs){
+                    //     console.log(log)
+                    //     web3.eth.getBlock(log['blockNumber'], function(err, data){
+                    //         console.log(data)
+                    //     })
+                    // }
+                } else {
+                    res.json({ 'message': 'Failed', 'reason': 'No logs available at the moment.' })
+                }
+            })
+    }else {
+    res
+        .status(500)
+        .json({
+            message: "Failed",
+            reason: "Contract Address is missing in the query"
+        });
+}
 });
 
 router.get("/starttime", function (req, res, next) {
@@ -164,6 +144,59 @@ router.get("/starttime", function (req, res, next) {
     }
 });
 
+router.get("/endtime", function (req, res, next) {
+    if ("address" in req.query) {
+        var votingContractAddress = req.query["address"];
+        if (votingContractAddress in global.contractInstances) {
+            var contractInstance = global.contractInstances[votingContractAddress];
+        } else {
+            var contractInstance = new web3.eth.Contract(abi, votingContractAddress);
+            global.contractInstances[votingContractAddress] = contractInstance;
+        }
+        contractInstance.methods.getEndTime().call(function (err, result) {
+            if (err) {
+                console.error(err);
+                res.status(500).json({ message: "Failed", reason: err });
+            }
+            res.json({ message: "Success", data: { endtime: result } });
+        });
+    } else {
+        res
+            .status(500)
+            .json({
+                message: "Failed",
+                reason: "Contract Address is missing in the query"
+            });
+    }
+});
+
+router.get("/totalvotes", function (req, res, next) {
+    if ("address" in req.query) {
+        var votingContractAddress = req.query["address"];
+        if (votingContractAddress in global.contractInstances) {
+            var contractInstance = global.contractInstances[votingContractAddress];
+        } else {
+            var contractInstance = new web3.eth.Contract(abi, votingContractAddress);
+            global.contractInstances[votingContractAddress] = contractInstance;
+        }
+        contractInstance.methods.getVoterBaseDenominator().call(function (err, result) {
+            if (err) {
+                console.error(err);
+                res.status(500).json({ message: "Failed", reason: err });
+            }
+            res.json({ message: "Success", data: { totalvotes: result } });
+        });
+    } else {
+        res
+            .status(500)
+            .json({
+                message: "Failed",
+                reason: "Contract Address is missing in the query"
+            });
+    }
+});
+
+
 router.get("/voterbaselogic", function (req, res, next) {
     if ("address" in req.query) {
         var votingContractAddress = req.query["address"];
@@ -178,7 +211,7 @@ router.get("/voterbaselogic", function (req, res, next) {
                 console.error(err);
                 res.status(500).json({ message: "Failed", reason: err });
             }
-            res.json({ message: "Success", data: { voterbaselogic: result } });
+            res.json({ message: "Success", data: { voterbaselogic: web3.utils.hexToAscii(result).replace(/\0/g, '') } });
         });
     } else {
         res
@@ -204,7 +237,7 @@ router.get("/votetallies", function (req, res, next) {
                 console.error(err);
                 res.status(500).json({ message: "Failed", reason: err });
             }
-            res.json({ message: "Success", data: { votetallies: result } });
+            res.json({ message: "Success", data: { votetallies: web3.utils.hexToAscii(result).replace(/\0/g, '') } });
         });
     } else {
         res
@@ -248,8 +281,7 @@ router.get("/proposalswithvotes", function (req, res, next) {
                                 if (proposalName.length > 0) {
                                     proposalsWithVotes.push({
                                         name: proposalName,
-                                        votes: result[proposal],
-                                        percent: 10
+                                        votes: result[proposal]
                                     });
                                     totalVotesCasted += parseInt(result[proposal]);
                                 }
